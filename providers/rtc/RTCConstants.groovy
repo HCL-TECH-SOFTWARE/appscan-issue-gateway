d1ad2fd41acaeb19458fab32889db2ea1c8b7311
Felipe Leite <felipe.leite@primeup.com.br>
2019-01-22 15:31:27 -0200

    Change string to def

--- providers/rtc/RTCConstants.groovy
+++ providers/rtc/RTCConstants.groovy
@@ -10,41 +10,41 @@ package rtc
  */
 public class RTCConstants {
 	
-	static final String ROOT_SERVICES = "/rootservices"
-	static final String JSECURITYCHECK = "/j_security_check"
+	static def ROOT_SERVICES = "/rootservices"
+	static def JSECURITYCHECK = "/j_security_check"
 
-	static final String AUTHREQUIRED_HEADER = "X-com-ibm-team-repository-web-auth-msg"
-	static final String AUTHREQUIRED_VALUE = "authrequired"
-	static final String AUTHFAILED_VALUE = "authfailed"
-	static final String BASIC_HEADER = "WWW-Authenticate"
-	static final String AUTHORIZATION_HEADER = "Authorization"
-	static final String APPLICATION_RDF_XML_TYPE = "application/rdf+xml"
-	static final String ACCEPT_HEADER = "Accept"
-	static final String CONTENT_TYPE_HEADER = "Content-Type"
-	static final String OSLC_CORE_VERSION_HEADER = "OSLC-Core-Version"
-	static final String OSLC_VERSION = "2.0"
-	static final String FILED_AGAINST_PROPERTY = "filedAgainst"
-	static final String FILE_NAME = "IssueDetails-%s.html"
-	static final String ATTACHMENT_UPLOAD_URL = "%s/service/com.ibm.team.workitem.service.internal.rest.IAttachmentRestService/?projectId=%s&multiple=true"
+	static def AUTHREQUIRED_HEADER = "X-com-ibm-team-repository-web-auth-msg"
+	static def AUTHREQUIRED_VALUE = "authrequired"
+	static def AUTHFAILED_VALUE = "authfailed"
+	static def BASIC_HEADER = "WWW-Authenticate"
+	static def AUTHORIZATION_HEADER = "Authorization"
+	static def APPLICATION_RDF_XML_TYPE = "application/rdf+xml"
+	static def ACCEPT_HEADER = "Accept"
+	static def CONTENT_TYPE_HEADER = "Content-Type"
+	static def OSLC_CORE_VERSION_HEADER = "OSLC-Core-Version"
+	static def OSLC_VERSION = "2.0"
+	static def FILED_AGAINST_PROPERTY = "filedAgainst"
+	static def FILE_NAME = "IssueDetails-%s.html"
+	static def ATTACHMENT_UPLOAD_URL = "%s/service/com.ibm.team.workitem.service.internal.rest.IAttachmentRestService/?projectId=%s&multiple=true"
 
 	//Provider name
-	static final String PROVIDER_NAME = "rtc"
+	static def PROVIDER_NAME = "rtc"
 
 	//Required fields
-	static final String SERVER_URL = "url"
-	static final String USERNAME = "username"
-	static final String PASSWORD = "password"
-	static final String PROJECTAREA = "projectarea"
-	static final String ISSUETYPE = "issuetype"
+	static def SERVER_URL = "url"
+	static def USERNAME = "username"
+	static def PASSWORD = "password"
+	static def PROJECTAREA = "projectarea"
+	static def ISSUETYPE = "issuetype"
 	
 	//Optional fields	
-	static final String OTHERFIELDS = "otherfields"
-	static final String FILEDAGAINST = "filedAgainst"
-	static final String SUMMARY = "summary"
-	static final String DESCRIPTION = "description"
+	static def OTHERFIELDS = "otherfields"
+	static def FILEDAGAINST = "filedAgainst"
+	static def SUMMARY = "summary"
+	static def  DESCRIPTION = "description"
 	
 	//Description
-	static final String PROVIDER_DESCRIPTION =
+	static def PROVIDER_DESCRIPTION =
 	[
 		'RTC provider. Configuration fields are below',
 		'(Required)' + SERVER_URL   + ': the RTC Server URL to connect to',
