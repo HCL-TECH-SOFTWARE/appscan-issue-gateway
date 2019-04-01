1edddb9b42f96cbbcdf80a8514489aba489dbcee
anurag <anurag-s@hcl.com>
2019-04-01 11:16:09 +0530

    to support different versions of APIs

---
+++ src/main/java/com/hcl/appscan/issuegateway/issues/ASOCPushJobData.java
@@ -0,0 +1,40 @@
+package com.hcl.appscan.issuegateway.issues;
+
+import java.util.Map;
+
+import io.swagger.annotations.ApiModelProperty;
+
+public class ASOCPushJobData {
+	public AppScanDataV1 appscanData;
+	public IMDataV1 imData;
+	
+	static public class AppScanDataV1 {
+		@ApiModelProperty(position = 1, required = false, value = "The root URL if connecting to any version of ASoC other than public cloud", example = "https://appscan.ibmcloud.com")
+		public String url = "https://appscan.ibmcloud.com";
+		@ApiModelProperty(position = 2, required = true, value = "Your API Key Id generated from Application Security on Cloud")
+		public String apikeyid;
+		@ApiModelProperty(position = 3, required = true, value = "Your API Key Secret generated from Application Security on Cloud")
+		public String apikeysecret;
+		@ApiModelProperty(position = 4, required = true, value = "The Id of the ASoC application to be processed")
+		public String appid;
+		@ApiModelProperty(position = 5, required = false, example = "25", value = "Maximum number of issues to process. The default is 25. Pass in a value of -1 to have no limit")
+		public Integer maxissues = 25;
+		@ApiModelProperty(position = 6, required = false, value = "Issue States to process.  The default is to only process issues that are 'Open'. To also process new issues use 'New, Open'")
+		public String issuestates = "Open";
+		@ApiModelProperty(position = 7, required = false, value = "Comma separated list of ASoC policy ids used to filter the results. If not specified, the application's registered policies will be used")
+		public String policyids;	
+		@ApiModelProperty(position = 8, required = false, value = "List of regex experessions to run on Issue fields, to further filter the results")
+		public Map<String, String> issuefilters;
+		@ApiModelProperty(position = 9, required = false, value = "Other internal debug or demo settings")
+		public Map<String, String> other;
+
+	}
+	
+	static public class IMDataV1 {
+		@ApiModelProperty(position = 1, required = true, value = "The Issue Management provider to use (e.g. 'jira')")
+		public String provider;
+		@ApiModelProperty(position = 2, required = true, value = "List of configuration settings to be used while processing the issues. See the help of the appropriate Issue Management provider for specifics")
+		public Map<String,Object> config;
+	}
+}
+
--- src/main/java/com/hcl/appscan/issuegateway/issues/PushJobController.java
+++ src/main/java/com/hcl/appscan/issuegateway/issues/PushJobController.java
@@ -24,16 +24,55 @@ import io.swagger.annotations.ApiParam;
 
 @RestController
 @Api(tags = "issues")
-@RequestMapping("/issues/pushjobs")
+
 public class PushJobController {
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
-    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+	@RequestMapping(value="/issues/pushjobs",method = RequestMethod.POST, produces = "application/json")
     @ApiOperation(notes="This API creates a Job that will process AppScan issues and push them into other issue management systems. "
     		+ "The job is completely controlled by the JSON that is passed in. The details of the JSON will vary depending on your target issue management system."
     		+ "To view configuration details, invoke the GET /providers API below and the details will be in the response",value="Create a job to push AppScan issues to an issue management system")
-	PushJobResult issuePush(@Valid @RequestBody @ApiParam(name="body", required=true) PushJobData submitJobData) {
+	PushJobResult issuePush(@Valid @RequestBody @ApiParam(name="body", required=true) ASOCPushJobData submitJobData) {
+    	try {
+    		logger.debug("New Request.  Payload:\n" + new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(submitJobData));
+    	} catch (Exception e) {logger.error("Error parsing JSON", e);}
+    	
+    	PushJobData jobData= new PushJobData();
+    	PushJobData.AppScanData appscanData= new PushJobData.AppScanData();
+    	PushJobData.IMData imData=new PushJobData.IMData();
+    	jobData.setAppscanData(appscanData);
+    	jobData.setImData(imData);
+    	jobData.getAppscanData().setAppscanProvider("ASOC");
+    	jobData.getAppscanData().setUrl(submitJobData.appscanData.url);
+    	jobData.getAppscanData().setApikeyid(submitJobData.appscanData.apikeyid);
+    	jobData.getAppscanData().setApikeysecret(submitJobData.appscanData.apikeysecret);
+    	jobData.getAppscanData().setAppid(submitJobData.appscanData.appid);
+    	jobData.getAppscanData().setMaxissues(submitJobData.appscanData.maxissues);
+    	jobData.getAppscanData().setIssuestates(submitJobData.appscanData.issuestates);
+    	jobData.getAppscanData().setPolicyids(submitJobData.appscanData.policyids);
+    	jobData.getAppscanData().setExcludeIssuefilters(submitJobData.appscanData.issuefilters);
+    	jobData.getImData().setProvider(submitJobData.imData.provider);
+    	jobData.getImData().setConfig(submitJobData.imData.config);
+    	
+    	PushJob submitJob = new PushJob(jobData);
+    	PushJobResult jobResult = JobManager.getInstance().submitJob(submitJob);
+   		return jobResult;
+    }
+    
+    @RequestMapping(value="/issues/pushjobs",method = RequestMethod.GET, produces = "application/json")
+    @ApiOperation(value="Get an issue push job",notes="A PushJob will have a Status of \"Running - Current Operation\" until the job has either finished successfully or failed. "
+    		+ "The errors field holds problems that were encountred during the operation. "
+    		+ "The results field will (if successful) hold a Map of AppScan Issue Ids and their associated Issues in the other issue management system.")
+	PushJobResult getStatus(String id) throws EntityNotFoundException{
+		return JobManager.getInstance().getJobResult(id);
+	}
+	
+    @RequestMapping(value="/v2/issues/pushjobs",method = RequestMethod.POST, produces = "application/json")
+    @ApiOperation(notes="This API creates a Job that will process AppScan issues and push them into other issue management systems. "
+    		+ "The job is completely controlled by the JSON that is passed in. The details of the JSON will vary depending on your target issue management system."
+    		+ "To view configuration details, invoke the GET /providers API below and the details will be in the response",value="Create a job to push AppScan issues to an issue management system")
+	PushJobResult issuePushV2(@Valid @RequestBody @ApiParam(name="body", required=true) PushJobData submitJobData) {
     	try {
     		logger.debug("New Request.  Payload:\n" + new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(submitJobData));
     	} catch (Exception e) {logger.error("Error parsing JSON", e);}
@@ -42,11 +81,11 @@ public class PushJobController {
    		return jobResult;
     }
     
-    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(value="/v2/issues/pushjobs",method = RequestMethod.GET, produces = "application/json")
     @ApiOperation(value="Get an issue push job",notes="A PushJob will have a Status of \"Running - Current Operation\" until the job has either finished successfully or failed. "
     		+ "The errors field holds problems that were encountred during the operation. "
     		+ "The results field will (if successful) hold a Map of AppScan Issue Ids and their associated Issues in the other issue management system.")
-	PushJobResult getStatus(String id) throws EntityNotFoundException{
+	PushJobResult getStatusV2(String id) throws EntityNotFoundException{
 		return JobManager.getInstance().getJobResult(id);
 	}
      
