99983a661dee0088d8cd508ea817c85bccd844d9
Mukeshkumarvi-jain <Mukeshkumarvi.jain@hcl.com>
2026-01-13 10:43:53 +0530

    Updated copyright years and POM version
    Extended HCL Technologies copyright years to 2026 in multiple source files and updated the project version to 1.2.2.

--- src/main/java/common/IAppScanIssue.java
+++ src/main/java/common/IAppScanIssue.java
@@ -1,6 +1,6 @@
 /**
  * � Copyright IBM Corporation 2018.
- * � Copyright HCL Technologies Ltd. 2018.
+ * � Copyright HCL Technologies Ltd. 2018,2026.
  * LICENSE: Apache License, Version 2.0 https://www.apache.org/licenses/LICENSE-2.0
  */
 package common;
